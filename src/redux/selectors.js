bf7cc731047d1eeaa11a43cce02de944c189cf14
dozeran <dozeran@proton.me>
2023-11-25 16:18:04 +0200

    update Selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,7 +2,7 @@ import { createSelector } from '@reduxjs/toolkit';
 
 export const selectContacts = state => state.contacts.items;
 export const selectFilter = state => state.filter;
-export const selectisLoading = state => state.isLoading;
+export const selectisLoading = state => state.contacts.isLoading;
 export const selectError = state => state.Error;
 
 export const selectVisibleContacts = createSelector(
